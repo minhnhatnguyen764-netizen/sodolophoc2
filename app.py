0b2faf4ab0631f43be67a27cf019d60d9556a507
minhnhatnguyen764-netizen <minhnhatnguyen764@gmail.com>
2026-09-23 23:55:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ with tab_sodo:
     st.markdown("BỤC GIẢNG / BÀN GIÁO VIÊN", unsafe_allow_html=True)
 
 def ve_cum_4_hoc_sinh(hs_day_a, hs_day_b, title):
-    with st.container(border=True):
+        with st.container(border=True):
         st.markdown(f"{title}", unsafe_allow_html=True)
 c1, c2, gap, c3, c4 = st.columns([1, 1, 0.3, 1, 1])
 def get_hs(hs_list, cho):
