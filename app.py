57c84f78c433e2cde9914fe15d87b6d73706d648
minhnhatnguyen764-netizen <minhnhatnguyen764@gmail.com>
2026-09-24 00:40:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ MAT_KHAU_QUAN_TRI = "12A1HIEN"
 NGAY_BAT_DAU = datetime(2026, 9, 23)
 FILE_DATA = "danh_sach_lop.csv"
 FILE_BG = "bg_url.jpg"
-bg_url = "https://i.postimg.cc/nh52Z3Ns/793089681-4483143295275332-798032956225024408-n.jpg"
+bg_url = "[![793089681-4483143295275332-798032956225024408-n.jpg](https://i.postimg.cc/nh52Z3Ns/793089681-4483143295275332-798032956225024408-n.jpg)](https://postimg.cc/LJtL339R)"
 
 if os.path.exists(FILE_BG):
     with open(FILE_BG, "r") as f:
